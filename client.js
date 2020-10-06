c18eb4570fd45b2797daffac0d412a3f884a2765
Sam Mahler <mahler.sam@gmail.com>
2020-10-06 11:39:48 -0500

    completed code along

--- client.js
+++ client.js
@@ -1,17 +1,30 @@
 console.log('Here are all the available people:', people);
 
+let randomPerson = '';
+
 $(document).ready(onReady);
 
 function onReady() {
     appendPeople();
     pickRandom();
+    $('#githubPeople').on('click', '.clickPic', clickedPerson)
+}
+
+function clickedPerson () {
+    let clickedPerson = $(this).data('name');
+    if (clickedPerson == randomPerson) {
+        alert(`Yes, this is ${randomPerson}!`);
+        pickRandom();
+    } else {
+        alert('Try Again!')
+    }
 }
 
 function appendPeople() {
     for (let i = 0; i < people.length; i++) {  
         $('#githubPeople').append(`
-            <li>
-                <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}">
+            <li data-name=${people[i].name} class="clickPic">
+                <img src="https://github.com/${people[i].githubUsername}.png?size=250" alt="Profile image of ${people[i].name}"/>
             </li>
         `);
     }
@@ -20,7 +33,9 @@ function appendPeople() {
 function pickRandom() {
     let randNum = randomNumber(0, people.length - 1);
 
-    $('#person').append(people[randNum].name);
+    randomPerson = people[randNum].name;
+
+    $('#person').empty().append(randomPerson);
 }
 
 function randomNumber(min, max){
